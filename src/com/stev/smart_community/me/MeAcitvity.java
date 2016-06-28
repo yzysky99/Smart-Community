fb5a0de9c6729be8214ba75bec61027ae18bbc7e
stevyang <stevyang@tencent.com>
2016-06-28 20:41:11 +0800

    add me page

--- src/com/stev/smart_community/me/MeAcitvity.java
+++ src/com/stev/smart_community/me/MeAcitvity.java
@@ -1,27 +1,51 @@
 package com.stev.smart_community.me;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.stev.smart_community.R;
+import com.stev.smart_community.home.NoticeAcitvity;
 
 public class MeAcitvity extends Fragment {
 
+	ImageView mUserAvatar;
+	TextView mUserID;
+	RelativeLayout mUserInfo;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
 		View view = LayoutInflater.from(getActivity()).inflate(R.layout.me_page, null);
 		initView(view);
+		initData();
 		return view;
 	}
 	
-
 	private void initView(View view) {
-		
+		mUserAvatar = (ImageView)view.findViewById(R.id.iv_user_avatar);
+		mUserID = (TextView) view.findViewById(R.id.tv_user_id);
+		mUserInfo = (RelativeLayout) view.findViewById(R.id.rl_user_info);
+	}
+	
+	private void initData() {
+//		mUserAvatar.setImageBitmap(bm);
+		mUserID.setText(getResources().getString(R.string.me_default_userid));
+		mUserInfo.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getActivity(), UserInfoAcitvity.class);
+				startActivity(intent);
+			}
+		});
 	}
 	
 	@Override
---
+++ src/com/stev/smart_community/me/UserInfoAcitvity.java
@@ -0,0 +1,34 @@
+package com.stev.smart_community.me;
+
+import com.stev.smart_community.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class UserInfoAcitvity extends Activity {
+	TextView mUserName;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.user_info);
+		mUserName = (TextView)findViewById(R.id.tv_user_name);
+		mUserName.setText(R.string.me_default_userid);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+
+}
